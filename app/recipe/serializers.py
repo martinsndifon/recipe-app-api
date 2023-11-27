7339a3123800f0d5e2f7b26b2e3e6bb823d94060
martinsndifon <martinsndifon@gmail.com>
2023-11-27 17:07:17 +0100

    write test for creating recipe

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -6,9 +6,16 @@ from core.models import Recipe
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-	"""Serializer for recipes."""
+    """Serializer for recipes."""
 
-	class Meta:
-		model = Recipe
-		fields = ['id', 'title', 'time_minutes', 'price', 'link']
-		read_only_fields = ['id']
+    class Meta:
+        model = Recipe
+        fields = ["id", "title", "time_minutes", "price", "link"]
+        read_only_fields = ["id"]
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for the recipe detail view"""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -10,12 +10,19 @@ from recipe import serializers
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
-	"""View for managing recipe APIs"""
-	serializer_class = serializers.RecipeSerializer
-	queryset = Recipe.objects.all()
-	authentication_classes = [TokenAuthentication]
-	permission_classes = [IsAuthenticated]
+    """View for managing recipe APIs"""
+    serializer_class = serializers.RecipeDetailSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
-	def get_queryset(self):
-		"""Retrieve recipes for authenticated user."""
-		return self.queryset.filter(user=self.request.user).order_by('-id')
+    def get_queryset(self):
+        """Retrieve recipes for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """Return the serializer class for request."""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
