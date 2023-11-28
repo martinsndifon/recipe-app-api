0cbc268b8d9956e72fd03528958da715cf158650
martinsndifon <martinsndifon@gmail.com>
2023-11-28 13:30:18 +0100

    Implemented the feature for deleting tags

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -40,7 +40,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
 
 class TagViewSet(
-    mixins.UpdateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
 ):
     """Manage tags in the database"""
 
