502ea0c07f5e361c85ca488022eee15fd74811cf
martinsndifon <martinsndifon@gmail.com>
2023-11-29 11:32:35 +0100

    Implemented lising ingredients

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -4,6 +4,7 @@ Serializers for recipe APIs
 
 from rest_framework import serializers
 from core.models import (
+    Ingredient,
     Recipe,
     Tag,
 )
@@ -18,14 +19,24 @@ class TagSerializer(serializers.ModelSerializer):
         read_only_fields = ["id"]
 
 
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for ingredients"""
+
+    class Meta:
+        model = Ingredient
+        fields = ["id", "name"]
+        read_only_fields = ["id"]
+
+
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
 
     tags = TagSerializer(many=True, required=False)
+    ingredients = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ["id", "title", "time_minutes", "price", "link", "tags"]
+        fields = ["id", "title", "time_minutes", "price", "link", "tags", "ingredients"]
         read_only_fields = ["id"]
 
     def _get_or_create_tags(self, tags, recipe):
